bd33d594f28bf48b5572339dadf984be9d084116
batmunkh namshir <namshir17@gmail.com>
2022-01-11 10:36:33 -0800

    commit tree 49d72c603b5720e2f30418f7ccbbf75d75590659

---
+++ index.js
@@ -0,0 +1,10 @@
+let receivesAFunction = (callback) => callback();
+
+
+function returnsANamedFunction () {
+ return  function names() {}
+}
+
+function  returnsAnAnonymousFunction() {
+   return function() {}
+}
